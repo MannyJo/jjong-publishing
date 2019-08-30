da244ba2b6abb1290c3fb5f7518abb80bb8d2196
Manny Jo <jomansang@gmail.com>
2019-08-30 11:22:43 -0500

    Add jQuery and functions to show an info popup

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
 console.log('wireframe.js');
 
+$(document).ready(function(){
+    clickRescBtn1();
+    clickRescBtn2();
+    clickRescBtn3();
+});
+
+const clickRescBtn1 = () => {
+    // When the button having a class name 'resvBtn1' is clicked, show an information popup.
+    // 클래스명이 'resvBtn1'인 버튼이 눌리면 정보창을 보여줘라. 
+    $('.resvBtn1').on('click', function(){
+        alert('대실예약하기 버튼 클릭');
+    });
+}
+
+const clickRescBtn2 = () => {
+    $('.resvBtn2').on('click', function(){
+        alert('숙박예약하기 버튼 클릭');
+    });
+}
+
+const clickRescBtn3 = () => {
+    $('.resvBtn3').on('click', function(){
+        alert('예약하기 버튼 클릭');
+    });
+}
